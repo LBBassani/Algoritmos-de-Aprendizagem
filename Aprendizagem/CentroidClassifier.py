20e84bada91ab6396b87ae151108888636abbcf2
LBBassani <lorenabassani12@gmail.com>
2019-11-12 13:54:02 -0300

    Inicio da implementação de CentroidClassifier

---
+++ Aprendizagem/CentroidClassifier.py
@@ -0,0 +1,20 @@
+import warnings
+def warn(*args, **kargs):
+    pass
+warnings.warn = warn
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from scipy.spatial import distance
+
+class CentroidClassifier(BaseEstimator, ClassifierMixin):
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        self.classes_ = unique_labels(y)
+        self.X_ = X
+        self.y_ = y
+        return self
+    
+    def predict(self, X):
+        pass
