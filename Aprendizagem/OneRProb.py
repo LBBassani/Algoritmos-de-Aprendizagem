ed3ab39a1f2f49829cb2b53662e02e274668945d
LBBassani <lorenabassani12@gmail.com>
2019-11-12 12:53:22 -0300

    Inicio da implementação de OneR Probabilistico

---
+++ Aprendizagem/OneRProb.py
@@ -0,0 +1,17 @@
+import warnings
+def warn(*args, **kargs):
+    pass
+warnings.warn = warn
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from .OneR import OneR
+
+class OneRProb(OneR, BaseEstimator, ClassifierMixin):
+    def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        self.classes_ = unique_labels(y)
+        self.X_ = X
+        self.y_ = y
+        return self
